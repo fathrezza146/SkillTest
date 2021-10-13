71e080838b5b6dc9e015e8f869f4f32e9cb43776
fathrezza146 <32872153+fathrezza146@users.noreply.github.com>
2021-10-13 14:58:11 +0700

    Add files via upload

--- Oauthlogin/go.mod
+++ Oauthlogin/go.mod
@@ -1,3 +1,12 @@
 module logingoogle
 
 go 1.17
+
+require (
+	cloud.google.com/go v0.97.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
+	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
--- Oauthlogin/main.go
+++ Oauthlogin/main.go
@@ -1,5 +1,76 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+func init() {
+	authGoogle = &oauth2.Config{
+		RedirectURL:  "http://localhost:4200/callback",
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+}
+
+var (
+	authGoogle  *oauth2.Config
+	statestring = "pseudo-random"
+)
+
+func main() {
+	http.HandleFunc("/", HandleMain)
+	http.HandleFunc("/login", GoogleLogin)
+	http.HandleFunc("/callback", Googlecallback)
+	http.ListenAndServe(":4200", nil)
+}
+
+func HandleMain(w http.ResponseWriter, r *http.Request) {
+	const htmlindeks = `<html><body><a href="/login">Log in with Google</a></body></html>`
+	fmt.Fprintf(w, htmlindeks)
+}
+
+func GoogleLogin(w http.ResponseWriter, r *http.Request) {
+	login := authGoogle.AuthCodeURL(statestring)
+	http.Redirect(w, r, login, http.StatusTemporaryRedirect)
+}
+
+func Googlecallback(w http.ResponseWriter, r *http.Request) {
+	called, err := infouser(r.FormValue("state"), r.FormValue("code"))
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	fmt.Fprintf(w, "Content: %s\n", called)
+}
+
+func infouser(state string, code string) ([]byte, error) {
+	if state != statestring {
+		return nil, fmt.Errorf("no oauthstate")
+	}
+
+	token, err := authGoogle.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		return nil, fmt.Errorf("tukeran code error : %s", err.Error())
+	}
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get user info: %s", err.Error())
+	}
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response bodt : %s", err.Error())
+	}
+	return contents, nil
 }
